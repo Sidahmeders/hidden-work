fa1a6924c8128a1662a7097d0d0e5e71fafa2151
Sidahmeders <ahorob57@gmail.com>
2022-01-30 21:22:32 +0100

    added the store and pubsub module

---
+++ src/utils/store/store.js
@@ -0,0 +1,67 @@
+import PubSub from '../lib/pubsub.js'
+
+export default class Store {
+  constructor(params) {
+    let self = this
+
+    self.proxyTrap = {
+      set: function (state, key, value) {
+        state[key] = value
+        console.log(`stateChange: ${key}: ${value}`)
+        self.events.publish('stateChange', self.state)
+
+        if (self.status !== 'mutation') {
+          console.warn(`You should use a mutation to set ${key}`)
+        }
+        self.status = 'resting'
+
+        return true
+      },
+    }
+
+    self.state = new Proxy(params.state || {}, self.proxyTrap)
+    self.actions = {}
+    self.mutations = {}
+    self.events = new PubSub()
+    self.status = 'resting'
+
+    if (Object.prototype.hasOwnProperty.call(params, 'actions')) {
+      self.actions = params.actions
+    }
+
+    if (Object.prototype.hasOwnProperty.call(params, 'mutations')) {
+      self.mutations = params.mutations
+    }
+  }
+
+  dispatch(actionKey, payload) {
+    let self = this
+
+    if (typeof self.actions[actionKey] !== 'function') {
+      console.error(`Action "${actionKey} doesn't exist.`)
+      return false
+    }
+
+    console.groupCollapsed(`ACTION: ${actionKey}`)
+    self.status = 'action'
+    self.actions[actionKey](self, payload)
+    console.groupEnd()
+
+    return true
+  }
+
+  commit(mutationKey, payload) {
+    let self = this
+
+    if (typeof self.mutations[mutationKey] !== 'function') {
+      console.log(`Mutation "${mutationKey}" doesn't exist`)
+      return false
+    }
+
+    self.status = 'mutation'
+    let newState = self.mutations[mutationKey](self.state, payload)
+    self.state = Object.assign(self.state, newState)
+
+    return true
+  }
+}
